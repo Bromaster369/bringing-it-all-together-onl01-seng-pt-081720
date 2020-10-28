bb9b17986effbb6d4c614554080a87b3c9f9aac3
Christian Pardo <chrispardopersonal@gmail.com>
2020-10-28 01:49:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -85,7 +85,8 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?",name, breed)
     if !dog.empty?
-      dog_data = 
+      dog_data = dog[0]
+      
     
   end 
   
